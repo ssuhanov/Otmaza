4854a6b81cd1e85d1240fb436412397d99d7692d
Serge Sukhanov <ssuhanov@icloud.com>
2016-02-12 21:17:42 +0200

    Added record to core data

---
+++ Otmaza/Otmaza.swift
@@ -0,0 +1,19 @@
+//
+//  Otmaza.swift
+//  Otmaza
+//
+//  Created by Serge Sukhanov on 12.02.16.
+//  Copyright © 2016 Serge Sukhanov. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Otmaza: NSManagedObject {
+
+    @NSManaged var id: NSNumber
+    @NSManaged var text: String
+    
+    
+}
