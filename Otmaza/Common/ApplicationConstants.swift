ab9cb49c70abd3e910553b43d8099c2f2d25b1f2
Serge Sukhanov <ssuhanov@icloud.com>
2018-11-28 12:11:47 +0200

    Implement service for otmaza's exctracting

---
+++ Otmaza/Common/ApplicationConstants.swift
@@ -0,0 +1,13 @@
+//
+//  ApplicationConstants.swift
+//  Otmaza
+//
+//  Created by Serge Sukhanov on 11/28/18.
+//  Copyright © 2018 Serge Sukhanov. All rights reserved.
+//
+
+import Foundation
+
+struct ApplicationConstants {
+    static let Blockquote = "blockquote"
+}
